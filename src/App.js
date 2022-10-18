b8a0994c336c47473b09430758ba544e280cc143
“Matt <matt.s.demartino@gmail.com>
2022-10-18 12:57:00 -0400

    Create Global Styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { GlobalStyle } from "./GlobalStyles";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
