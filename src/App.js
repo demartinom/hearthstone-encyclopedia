149689877b6aa13bea0b91e32f876483d731ff60
“Matt <matt.s.demartino@gmail.com>
2022-10-18 13:05:04 -0400

    Add Home to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { GlobalStyle } from "./GlobalStyles";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Home />
     </div>
   );
 }
