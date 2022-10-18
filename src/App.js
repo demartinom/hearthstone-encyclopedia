766a0d7b90ac073b5e26b87aa9255e11a00e98de
“Matt <matt.s.demartino@gmail.com>
2022-10-18 13:21:38 -0400

    Add metadata state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { GlobalStyle } from "./GlobalStyles";
 import Home from "./components/Home/Home";
+import { options } from "./API-Options";
 import { Routes, Route } from "react-router-dom";
-function App() {
 
+function App() {
+  const [metadata, setMetadata] = React.useState(null);
+  React.useState(() => {
+    fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/info", options)
+      .then((response) => response.json())
+      .then((response) => setMetadata(response))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <div className="App">
       <GlobalStyle />
