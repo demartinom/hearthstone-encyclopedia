de3af76f5f98bb93a0c381db7676825ecf311204
“Matt <matt.s.demartino@gmail.com>
2022-10-18 13:21:16 -0400

    Add Routes and Home Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { GlobalStyle } from "./GlobalStyles";
 import Home from "./components/Home/Home";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
+
   return (
     <div className="App">
       <GlobalStyle />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
