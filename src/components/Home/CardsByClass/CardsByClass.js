d4124deda39d4e62fc729b9d7d15f24ef3125447
“Matt <matt.s.demartino@gmail.com>
2022-10-18 13:50:55 -0400

    Map through list of classes

--- src/components/Home/CardsByClass/CardsByClass.js
+++ src/components/Home/CardsByClass/CardsByClass.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function CardsByClass(props) {
-  return <div></div>;
+  const classesList = props.classes.map((gameClass) => <h1 key={gameClass}>{gameClass}</h1>);
+  return <div>
+    {classesList}
+  </div>;
 }
