e2388857ce369f1fd1f5d69f58007cde5569d9a7
“Matt <matt.s.demartino@gmail.com>
2022-10-18 13:04:30 -0400

    Create API options

---
+++ src/API-Options.js
@@ -0,0 +1,7 @@
+export const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "df110cbc06msh276bdc41bf08ef9p163c3ajsne9ff8ac94d17",
+      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
