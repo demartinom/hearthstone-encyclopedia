5db12dac5fcd8f9054fa4feb1f2e2bd9c41e35ea
“Matt <matt.s.demartino@gmail.com>
2022-10-18 13:04:42 -0400

    Create Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hearthstone Encyclopedia</h1>
+      <h2>
+        Welcome to Hearthstone Encyclopedia! Here you can find a database of all
+        cards found within the game of Hearthstone. <br />
+        You can begin by searching for cards by set, by class, or by using the
+        advanced search.
+      </h2>
+      <a href="#">Find Cards by Set</a>
+      <a href="#">Find Cards by Class</a>
+    </div>
+  );
+}
