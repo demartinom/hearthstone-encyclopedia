da01099ab04af6a4a78e0437f47a1299a6109634
“Matt <matt.s.demartino@gmail.com>
2022-10-19 11:29:42 -0400

    Save api call in state

--- src/components/SpecificCard/SpecificCard.js
+++ src/components/SpecificCard/SpecificCard.js
@@ -3,12 +3,20 @@ import { options } from "../../API-Options";
 import { useParams } from "react-router-dom";
 
 export default function SpecificCard() {
-  const {name} = useParams()
-React.useEffect(()=>{
-  fetch(`https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${name}?collectible=1`, options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
-},[])
-  return <div></div>;
+  const { name } = useParams();
+  const [cardData, setCardData] = React.useState(null);
+  React.useEffect(() => {
+    fetch(
+      `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${name}?collectible=1`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setCardData(response))
+      .catch((err) => console.error(err));
+  }, []);
+  if (cardData === null) {
+    return <h1>Loading</h1>;
+  } else {
+    return <div></div>;
+  }
 }
