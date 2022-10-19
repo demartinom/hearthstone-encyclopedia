9031fdaa546ea5e328fd3fd63293939022d3a295
“Matt <matt.s.demartino@gmail.com>
2022-10-19 11:24:54 -0400

    Create useEffect with template card api call

--- src/components/SpecificCard/SpecificCard.js
+++ src/components/SpecificCard/SpecificCard.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { options } from "../../API-Options";
 
 export default function SpecificCard() {
+React.useEffect(()=>{
+  fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/loatheb", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+},[])
   return <div></div>;
 }
