c832b4c3fe99f80d5fc63a9d147d071acf94b303
“Matt <matt.s.demartino@gmail.com>
2022-10-19 13:56:20 -0400

    Map over set names with links provided

--- src/components/CardBySet/CardBySet.js
+++ src/components/CardBySet/CardBySet.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function CardBySet() {
-  return <div></div>;
+export default function CardBySet(props) {
+  const noDuplicates = [
+    ...new Map(props.sets.map((setName) => [setName, setName])).values(),
+  ];
+  const setList = noDuplicates.map((set) => (
+    <Link key={set} to={`/sets/${set}`}>
+      <h1>{set}</h1>
+    </Link>
+  ));
+  return <div>{setList}</div>;
 }
