763f6571ecb84567aea69b1f146438335fd34be0
“Matt <matt.s.demartino@gmail.com>
2022-10-19 14:02:23 -0400

    Create API call for specific set

--- src/components/CardBySet/SpecificSet.js
+++ src/components/CardBySet/SpecificSet.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { options } from "../../API-Options";
+import { useParams } from "react-router-dom";
 
 export default function SpecificSet() {
+  const { set } = useParams();
+  React.useEffect(() => {
+    fetch(
+      `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/sets/${set}?collectible=1`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  });
   return <div></div>;
 }
