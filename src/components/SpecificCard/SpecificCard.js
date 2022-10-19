bb1b05360f2bfb7209eb4a741ec5263b8db2b4a1
“Matt <matt.s.demartino@gmail.com>
2022-10-19 11:28:01 -0400

    Create api call for specific card page

--- src/components/SpecificCard/SpecificCard.js
+++ src/components/SpecificCard/SpecificCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { options } from "../../API-Options";
+import { useParams } from "react-router-dom";
 
 export default function SpecificCard() {
+  const {name} = useParams()
 React.useEffect(()=>{
-  fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/loatheb", options)
+  fetch(`https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${name}?collectible=1`, options)
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
