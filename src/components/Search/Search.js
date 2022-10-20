e0c8e40168449940bffa578a4c97e9b1c8372ff7
“Matt <matt.s.demartino@gmail.com>
2022-10-20 13:20:08 -0400

    Add map of searchResults

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,6 +4,7 @@ import { options } from "../../API-Options";
 export default function Search() {
   const [search, setSearch] = React.useState("");
   const [buttonClicked, setButtonClicked] = React.useState(false);
+  const [results, setResults] = React.useState([]);
   React.useEffect(() => {
     if (buttonClicked === true) {
       fetch(
@@ -11,7 +12,7 @@ export default function Search() {
         options
       )
         .then((response) => response.json())
-        .then((response) => console.log(response))
+        .then((response) => setResults(response))
         .then(setButtonClicked(false))
         .catch((err) => console.error(err));
     }
@@ -22,11 +23,15 @@ export default function Search() {
   function changeButton() {
     setButtonClicked(true);
   }
+  const searchResults = results.map((card) => (
+    <img src={card.img} alt="" key={card.cardId}></img>
+  ));
   return (
     <div>
       <h1>Find a card</h1>
       <input type="search" name="Search" id="Search" onChange={updateSearch} />
       <button onClick={changeButton}>Search</button>
+      {results.length > 0 && searchResults}
     </div>
   );
 }
