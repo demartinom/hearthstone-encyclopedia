7bcad0e77be361af331ae8183c9c1408a4caced2
“Matt <matt.s.demartino@gmail.com>
2022-10-20 14:51:35 -0400

    Moved CardsByClass to correct location

--- src/components/CardsByClass/SpecificClass.js
+++ src/components/CardsByClass/SpecificClass.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { options } from "../../../API-Options";
+import { options } from "../../API-Options";
 import { CardContainer, ClassCards } from "./CardsByClass.styled";
 import { Link } from "react-router-dom";
 
