19d4205d95b20ccdcd404c13cf959d00caf7a54d
“Matt <matt.s.demartino@gmail.com>
2022-10-20 13:25:40 -0400

    Add link to card

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { options } from "../../API-Options";
+import { Link } from "react-router-dom";
 
 export default function Search() {
   const [search, setSearch] = React.useState("");
@@ -24,7 +25,9 @@ export default function Search() {
     setButtonClicked(true);
   }
   const searchResults = results.map((card) => (
-    <img src={card.img} alt="" key={card.cardId}></img>
+    <Link key={card.cardId} to={`/${card.playerClass}/${card.name}`}>
+      <img src={card.img} alt="" />
+    </Link>
   ));
   return (
     <div>
