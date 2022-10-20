a21fec481423a2a601346d2d65ea9c86b26faf81
“Matt <matt.s.demartino@gmail.com>
2022-10-20 14:49:10 -0400

    Create and import NavBar styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Headings, NavBarStyled } from "./NavBar.styled";
 
 export default function NavBar() {
   return (
-    <div>
+    <NavBarStyled>
       <p>HearthStone Encyclopedia</p>
-      <div>
+      <Headings>
         <p>Sets</p>
         <p>Classes</p>
         <p>Search</p>
         <p>Favorites</p>
-      </div>
-    </div>
+      </Headings>
+    </NavBarStyled>
   );
 }
---
+++ src/components/NavBar/NavBar.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const NavBarStyled = styled.nav`
+  display: flex;
+`;
+
+export const Headings = styled.ul`
+  display: flex;
+  list-style: none;
+`;
