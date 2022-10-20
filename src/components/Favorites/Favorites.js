bfb92e645e6458bf1caa0d6bcf8bbee97c6f064f
“Matt <matt.s.demartino@gmail.com>
2022-10-20 12:21:56 -0400

    Create map of favorites including link to card

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Favorites(props) {
-  if (props.favorites === "") {
+  if (props.favorites === []) {
     return <h1>No favorites yet!</h1>;
   } else {
-    return <div></div>;
+    const favoriteCards = props.favorites.map((card) => (
+      <Link key={card.cardId} to={`/${card.playerClass}/${card.name}`}>
+        <img src={card.img} alt="" />
+      </Link>
+    ));
+    return <div>{favoriteCards}</div>;
   }
 }
