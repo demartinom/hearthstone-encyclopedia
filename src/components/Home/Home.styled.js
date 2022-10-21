6c82173c1e8e68f60dffd12c4d0cb3b361730484
“Matt <matt.s.demartino@gmail.com>
2022-10-21 12:12:42 -0400

    Create HomeLinks and add flex display to HomeStyle

--- src/components/Home/Home.styled.js
+++ src/components/Home/Home.styled.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
 export const HomeStyled = styled.div`
-font-family: var(--font-hearthstone);
+  font-family: var(--font-hearthstone);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const HomeLinks = styled.div`
+  display: flex;
+  gap: 1rem;
 `;
