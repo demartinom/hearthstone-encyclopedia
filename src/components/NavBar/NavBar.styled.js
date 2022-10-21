6dc98480ca8708b2139cd87511cdb6aaa8a0ce68
“Matt <matt.s.demartino@gmail.com>
2022-10-21 12:17:28 -0400

    Add text spacing

--- src/components/NavBar/NavBar.styled.js
+++ src/components/NavBar/NavBar.styled.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const NavBarStyled = styled.nav`
   display: flex;
   height: 4rem;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem;
 `;
 
 export const Headings = styled.ul`
