8fa7d22607ed8f106de033c9f0ea3e5ddb05b68c
“Matt <matt.s.demartino@gmail.com>
2022-10-24 12:58:51 -0400

    Add text to copy

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
       <h1>Hearthstone Encyclopedia</h1>
       <h2>
         Welcome to Hearthstone Encyclopedia! Here you can find a database of all
-        cards found within the game of Hearthstone. <br />
+        cards found within the game of Hearthstone produced by Blizzard Entertainment. <br />
         You can begin by searching for cards by set, by class, or by using the
         advanced search.
       </h2>
