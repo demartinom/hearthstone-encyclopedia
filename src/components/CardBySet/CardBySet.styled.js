30e421a6c9fe01ef0873a0d65f664a990ba1cf38
“Matt <matt.s.demartino@gmail.com>
2022-10-24 14:56:05 -0400

    Add further styling

--- src/components/CardBySet/CardBySet.styled.js
+++ src/components/CardBySet/CardBySet.styled.js
@@ -4,9 +4,13 @@ import { Link } from "react-router-dom";
 export const SetGallery = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  grid-gap: 1.75rem;
+  text-align: center;
+  align-items: center;
+  padding: .5rem 2rem;
 `;
 
 export const SetName = styled(Link)`
-    font-size: 1rem;
- 
+  font-size: 2rem;
+  font-family: var(--font-hearthstone);
 `;
