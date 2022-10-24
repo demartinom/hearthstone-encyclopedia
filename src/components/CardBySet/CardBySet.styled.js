40c9da7394e665e7f686e078a1d8f46b1cdaa3f8
“Matt <matt.s.demartino@gmail.com>
2022-10-24 14:31:40 -0400

    Begin styling

--- src/components/CardBySet/CardBySet.styled.js
+++ src/components/CardBySet/CardBySet.styled.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-export const SetGallery = styled.div``;
+export const SetGallery = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`;
 
-export const SetName = styled.div``;
+export const SetName = styled(Link)`
+    font-size: 1rem;
+ 
+`;
