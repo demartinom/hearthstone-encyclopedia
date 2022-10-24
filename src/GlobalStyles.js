514f1d84df0a2c9ca428f2f46deadb5c6bf2dfb5
“Matt <matt.s.demartino@gmail.com>
2022-10-24 12:55:13 -0400

    Add parchment background for body

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
-
 export const GlobalStyle = createGlobalStyle`
 *{
     box-sizing: border-box;
@@ -14,6 +13,9 @@ export const GlobalStyle = createGlobalStyle`
     --color-regular-blue: #7490BFff;
     --color-light-blue: #7BE2FDff;
     --color-brown: #6B4A28ff;        
+}
+body{
+  background-image: url("https://images.unsplash.com/photo-1601662528567-526cd06f6582?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1915&q=80");
 }
 `;
 
