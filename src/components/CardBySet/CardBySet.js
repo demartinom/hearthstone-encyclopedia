c370200613df17e6876f3dc61bec997fd56d9c02
“Matt <matt.s.demartino@gmail.com>
2022-10-24 14:31:35 -0400

    Move Link to styling for use in styledcomponents

--- src/components/CardBySet/CardBySet.js
+++ src/components/CardBySet/CardBySet.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { SetGallery, SetName } from "./CardBySet.styled";
 
 export default function CardBySet(props) {
@@ -7,10 +6,8 @@ export default function CardBySet(props) {
     ...new Map(props.sets.map((setName) => [setName, setName])).values(),
   ];
   const setList = noDuplicates.map((set) => (
-    <SetName>
-      <Link key={set} to={`/sets/${set}`}>
-        {set}
-      </Link>
+    <SetName key={set} to={`/sets/${set}`}>
+      {set}
     </SetName>
   ));
   return <SetGallery>{setList}</SetGallery>;
