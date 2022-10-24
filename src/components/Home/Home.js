90c7476a74a09582e1b9ae473b62738903942ab1
“Matt <matt.s.demartino@gmail.com>
2022-10-24 12:54:47 -0400

    import Hearthstone logo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { HomeStyled, HomeLinks } from "./Home.styled";
-
+import logo from "../../images/logo.png";
 export default function Home() {
   return (
     <HomeStyled>
+      <img src={logo} alt="hearthstone logo" />
       <h1>Hearthstone Encyclopedia</h1>
       <h2>
         Welcome to Hearthstone Encyclopedia! Here you can find a database of all
