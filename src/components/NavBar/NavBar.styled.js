882cf7ae8e3fa0060f8d262eff27332c192fcc43
“Matt <matt.s.demartino@gmail.com>
2022-10-24 13:53:33 -0400

    Add font and color

--- src/components/NavBar/NavBar.styled.js
+++ src/components/NavBar/NavBar.styled.js
@@ -6,9 +6,14 @@ export const NavBarStyled = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: 0 2rem;
+  font-family: var(--font-serif);
+  font-weight: 900;
+  font-size: 1.25rem;
+  color: var(--color-brown)
 `;
 
 export const Headings = styled.ul`
   display: flex;
   list-style: none;
+  gap: 1.5rem;
 `;
