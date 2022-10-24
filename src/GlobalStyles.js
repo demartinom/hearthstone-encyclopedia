bcd90b4316829241ec7a38f3bfd78db7f0d21dc7
“Matt <matt.s.demartino@gmail.com>
2022-10-24 13:14:42 -0400

    import parchment background

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
+import background from "./images/background.png";
+
 export const GlobalStyle = createGlobalStyle`
 *{
     box-sizing: border-box;
@@ -15,7 +17,7 @@ export const GlobalStyle = createGlobalStyle`
     --color-brown: #6B4A28ff;        
 }
 body{
-  background-image: url("https://images.unsplash.com/photo-1601662528567-526cd06f6582?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1915&q=80");
+  background-image: url(${background});
 }
 `;
 
