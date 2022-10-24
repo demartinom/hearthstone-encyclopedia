db7dd2e5c157f69d39953b58e23d83783d201f8a
“Matt <matt.s.demartino@gmail.com>
2022-10-24 13:43:04 -0400

    Change text description of buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,16 +8,15 @@ export default function Home() {
       <img src={logo} alt="hearthstone logo" />
       <h1>Hearthstone Encyclopedia</h1>
       <BodyText>
+        <h2>Welcome to Hearthstone Encyclopedia! </h2>
         <h2>
-          Welcome to Hearthstone Encyclopedia! Here you can find a database of
-          all cards found within the game of Hearthstone produced by Blizzard
-          Entertainment.
-        </h2>
-        <h2>
-          You can begin by searching for cards by set, by class, or by using the
-          advanced search.
+          Here you can find a database of all cards found within the game of
+          Hearthstone produced by Blizzard Entertainment.
         </h2>
       </BodyText>
+      <h2>
+        Use the buttons below to begin discovering the world of Hearthstone.
+      </h2>
       <HomeLinks>
         <Link to="/sets">Find Cards by Set</Link>
         <Link to="/classes">Find Cards by Class</Link>
