b6fcf2dd1eef75f0f6c699611c9359079b10d81c
“Matt <matt.s.demartino@gmail.com>
2022-10-25 12:50:03 -0400

    Add styling to set list

--- src/components/CardBySet/CardBySet.styled.js
+++ src/components/CardBySet/CardBySet.styled.js
@@ -4,13 +4,17 @@ import { Link } from "react-router-dom";
 export const SetGallery = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  grid-gap: 1.75rem;
-  text-align: center;
-  align-items: center;
-  padding: .5rem 2rem;
+  grid-gap: 2.5rem;
+  padding: 0.5rem 2rem;
 `;
 
 export const SetName = styled(Link)`
   font-size: 2rem;
   font-family: var(--font-hearthstone);
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  img {
+    height: 4rem;
+  }
 `;
