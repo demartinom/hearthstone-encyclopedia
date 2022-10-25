c3efac83f16a67247f88f6b2e28409c2640fe1ba
“Matt <matt.s.demartino@gmail.com>
2022-10-25 14:44:56 -0400

    Card styling

--- src/components/SpecificCard/SpecificCard.styled.js
+++ src/components/SpecificCard/SpecificCard.styled.js
@@ -2,9 +2,30 @@ import styled from "styled-components";
 
 export const Card = styled.div`
   display: flex;
+  align-items: center;
+  margin-left: 8rem;
+  margin-top: 3rem;
+  justify-content: center;
 `;
 
 export const CardInfo = styled.div`
   display: flex;
   flex-direction: column;
+  max-width: 25rem;
+
+  h1 {
+    font-family: var(--font-hearthstone);
+    font-size: 3rem;
+  }
+  h2,
+  p {
+    font-family: var(--font-merriweather);
+    font-weight: 900;
+  }
+  h2 {
+    font-size: 1.5rem;
+  }
+  p {
+    font-size: 1.25rem;
+  }
 `;
