a3b31ff1bc949b8cbd067cdadb2beb0e35033de8
“Matt <matt.s.demartino@gmail.com>
2022-10-25 13:44:53 -0400

    Create CardsByClass styling

--- src/components/CardsByClass/CardsByClass.styled.js
+++ src/components/CardsByClass/CardsByClass.styled.js
@@ -1,8 +1,22 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export const ClassList = styled.div``;
+export const CardsByClassStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: var(--font-hearthstone);
+  h1 {
+    margin-top: 1rem;
+    font-size: 2.5rem;
+  }
+`;
 
-export const CardsByClassStyled = styled.div``;
+export const ClassList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+  padding: 0.5rem 2rem;
+`;
 
 export const CardContainer = styled(Link)``;
