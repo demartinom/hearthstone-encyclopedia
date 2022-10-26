13c95a72436b56e2e940cc30014f12d91774b356
“Matt <matt.s.demartino@gmail.com>
2022-10-26 15:21:12 -0400

    Add styling for h1, h2, and button

--- src/components/Favorites/Favorites.styled.js
+++ src/components/Favorites/Favorites.styled.js
@@ -1,3 +1,29 @@
 import styled from "styled-components";
 
-export const FavoritesStyled = styled.div``;
+export const FavoritesStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    font-family: var(--font-hearthstone);
+    font-size: 3rem;
+  }
+  h2 {
+    margin-top: 10rem;
+    font-family: var(--font-hearthstone);
+    font-size: 3rem;
+  }
+  button {
+    border: none;
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+    margin: 1rem 0rem 0 1.5rem;
+    background: var(--color-brown);
+    color: var(--color-gold);
+    font-size: 1.5rem;
+    font-family: var(--font-merriweather);
+    font-weight: 900;
+    align-self: flex-start;
+  }
+`;
