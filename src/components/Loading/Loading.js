0d4b3316e5c6fc04eec38e3bc5264813814b8a7d
“Matt <matt.s.demartino@gmail.com>
2022-10-26 12:30:02 -0400

    Import LoadingStyled

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,11 @@
 import React from "react";
 import logo from "../../images/round-logo.png";
+import { LoadingStyled } from "./Loading.styling";
 
 export default function Loading() {
   return (
-    <div>
+    <LoadingStyled>
       <img src={logo} alt="" />
-    </div>
+    </LoadingStyled>
   );
 }
