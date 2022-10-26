9310faa56b43423a513776192b22cf4821e92734
“Matt <matt.s.demartino@gmail.com>
2022-10-26 13:38:43 -0400

    Style search bar

--- src/components/Search/Search.styled.js
+++ src/components/Search/Search.styled.js
@@ -13,15 +13,27 @@ export const StyledSearch = styled.div`
   input {
     border: none;
     width: 23rem;
-    height: 2rem;
+    height: 2.25rem;
     font-size: 1.75rem;
     font-weight: 100;
   }
 `;
 
-export const SearchFeature = styled.div``;
+export const SearchFeature = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  gap: 0.5rem;
+`;
 
 export const Icon = styled.div`
+  background-color: var(--color-brown);
+  border-radius: 0.25rem;
+  width: 3rem;
+  height: 2.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   :hover {
     cursor: pointer;
   }
