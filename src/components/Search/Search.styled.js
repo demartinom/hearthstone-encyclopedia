e3bde0197836afd4d130ee0316e79b4b7b606101
“Matt <matt.s.demartino@gmail.com>
2022-10-26 13:05:31 -0400

    Add styling

--- src/components/Search/Search.styled.js
+++ src/components/Search/Search.styled.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
 export const StyledSearch = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 1rem;
+  h1 {
+    font-family: var(--font-hearthstone);
+    font-size: 2.5rem;
+    margin-bottom: 1rem;
+  }
+  input {
+    border: none;
+    width: 23rem;
+    height: 2rem;
+    font-size: 1.75rem;
+    font-weight: 100;
+  }
+  button {
+  
+  }
 `;
