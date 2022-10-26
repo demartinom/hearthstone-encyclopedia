ad2d3be9e4f55bec48635f13f5f72dc3e5bfb565
“Matt <matt.s.demartino@gmail.com>
2022-10-26 12:25:23 -0400

    Import round logo

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,5 +1,10 @@
 import React from "react";
+import logo from "../../images/round-logo.png";
 
 export default function Loading() {
-  return <div></div>;
+  return (
+    <div>
+      <img src={logo} alt="" />
+    </div>
+  );
 }
