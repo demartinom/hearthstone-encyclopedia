6dbea2e69077f2f075183ca3bed20ffc9a7fa472
“Matt <matt.s.demartino@gmail.com>
2023-04-25 14:51:52 -0400

    Refresh token on page load

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,3 +1,13 @@
-export default function Home() {
+import React from "react";
+import axios from "axios";
+
+export default function Home({ test }) {
   return <div></div>;
+}
+
+export async function getServerSideProps() {
+  axios.post(
+    `https://us.battle.net/oauth/token?grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+  );
+  return { props: { test: "" } };
 }
