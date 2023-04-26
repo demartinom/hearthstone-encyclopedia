4635bd9a5aeddba5d81d27f30c36cf1456b78ecd
“Matt <matt.s.demartino@gmail.com>
2023-04-26 15:53:13 -0400

    Correct alignment of Nav items on mobile

--- next/components/navBar.js
+++ next/components/navBar.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="flex justify-between px-3 md:px-7 py-4 text-hlOrange bg-navBarBackground bg-cover md:text-xl font-uncial">
+    <nav className="flex items-center justify-between px-3 md:px-7 py-4 text-hlOrange bg-navBarBackground bg-cover md:text-xl font-uncial">
       <h3 className="w-20 md:w-auto">Hearthstone Encyclopedia</h3>
-      <li className="flex w-36 md:w-auto gap-2 flex-wrap lg:gap-5">
+      <li className="flex justify-around w-36 md:w-auto gap-x-4 flex-wrap lg:gap-5">
         <ul>Sets</ul>
         <ul>Classes</ul>
         <ul>Search</ul>
