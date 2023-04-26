101c755ff0cfc3ba9575be0aa798cf3fdbca3bc8
“Matt <matt.s.demartino@gmail.com>
2023-04-26 13:37:25 -0400

    Create NavBar html

---
+++ next/components/navBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <h3>Hearthstone Encyclopedia</h3>
+      <li>
+        <ul>Sets</ul>
+        <ul>Classes</ul>
+        <ul>Search</ul>
+        <ul>Favorites</ul>
+      </li>
+    </nav>
+  );
+};
+
+export default NavBar;
