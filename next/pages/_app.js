1c434751e0c72fe3872ce6a13751577b250cfe1b
“Matt <matt.s.demartino@gmail.com>
2023-04-26 14:35:29 -0400

    Import Uncial Font

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import localFont from "@next/font/local";
+
+const uncialFont = localFont({
+  src: "../public/lhf_uncial_caps-webfont.woff2",
+  weight: "400",
+  variable: "--font-uncial",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={uncialFont.variable}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
