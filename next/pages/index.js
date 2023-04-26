6640470e6ee4ce7f6d4e26ef19c4a5de85aab4d3
“Matt <matt.s.demartino@gmail.com>
2023-04-26 16:01:04 -0400

    Create home page

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import axios from "axios";
 import NavBar from "@/components/navBar";
+import Image from "next/image";
 
 export default function Home({ test }) {
   return (
-    <>
+    <div className="bg-hBeige min-h-screen">
       <NavBar />
-    </>
+      <div className="flex flex-col items-center py-12 px-16 md:px-48 md:py-20 gap-6 font-uncial text-center text-hBrown">
+        <Image
+          className="drop-shadow-xl"
+          src="/logo.png"
+          width={325}
+          height={325}
+        ></Image>
+        <h1 className="text-5xl md:text-6xl">Hearthstone Encyclopedia</h1>
+        <h3 className="text-xl md:text-3xl">
+          Here you can find a database of all cards found within the game of
+          Hearthstone produced by Blizzard Entertainment.
+        </h3>
+      </div>
+    </div>
   );
 }
 
