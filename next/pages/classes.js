fc7e77013e543bc4668a6c00058b00341acac871
“Matt <matt.s.demartino@gmail.com>
2023-04-27 16:30:04 -0400

    Get classes from API

--- next/pages/classes.js
+++ next/pages/classes.js
@@ -1,12 +1,20 @@
 import React from "react";
 import NavBar from "@/components/navBar";
+import axios from "axios";
 
-const Classes = () => {
+const Classes = ({ classData }) => {
   return (
     <div className="bg-hBeige min-h-screen">
       <NavBar />
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const res = await axios.get(
+    `https://us.api.blizzard.com/hearthstone/metadata/classes?locale=en_US&access_token=${process.env.API_TOKEN}`
+  );
+  return { props: { classData: JSON.parse(JSON.stringify(res.data)) } };
+}
 
 export default Classes;
