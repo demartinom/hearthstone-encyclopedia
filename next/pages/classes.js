1b714ae9c1193385fe3bda5f0f556578b125fc9b
“Matt <matt.s.demartino@gmail.com>
2023-04-27 16:32:03 -0400

    Change api call to getStaticProps

--- next/pages/classes.js
+++ next/pages/classes.js
@@ -3,14 +3,18 @@ import NavBar from "@/components/navBar";
 import axios from "axios";
 
 const Classes = ({ classData }) => {
+  const classList = classData.map((gameClass) => (
+      <li>{gameClass.name}</li>
+  ));
   return (
     <div className="bg-hBeige min-h-screen">
       <NavBar />
+      <ul>{classList}</ul>
     </div>
   );
 };
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await axios.get(
     `https://us.api.blizzard.com/hearthstone/metadata/classes?locale=en_US&access_token=${process.env.API_TOKEN}`
   );
