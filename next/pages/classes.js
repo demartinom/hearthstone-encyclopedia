ec7057dafb6783a7d37084facad64b61f83f7602
“Matt <matt.s.demartino@gmail.com>
2023-04-27 16:34:25 -0400

    Add class logos to list of classes

--- next/pages/classes.js
+++ next/pages/classes.js
@@ -1,15 +1,28 @@
 import React from "react";
 import NavBar from "@/components/navBar";
 import axios from "axios";
+import Link from "next/link";
+import Image from "next/image";
 
 const Classes = ({ classData }) => {
   const classList = classData.map((gameClass) => (
+    <Link href={`/classes/${gameClass.slug}`}>
       <li>{gameClass.name}</li>
+      {gameClass.slug !== "neutral" && (
+        <Image
+          width={100}
+          height={100}
+          src={`/class-logos/${gameClass.slug}.png`}
+        ></Image>
+      )}
+    </Link>
   ));
   return (
     <div className="bg-hBeige min-h-screen">
       <NavBar />
-      <ul>{classList}</ul>
+      <ul className="items-center inline-grid grid-cols-3 text-center gap-6 font-uncial">
+        {classList}
+      </ul>
     </div>
   );
 };
