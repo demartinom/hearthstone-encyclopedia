21d821b66bcfd15cbbddc9535f018ee6264668cf
“Matt <matt.s.demartino@gmail.com>
2023-04-27 16:36:19 -0400

    Create dynamic pages for classes

---
+++ next/pages/classes/[slug].js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "@/components/navBar";
+import axios from "axios";
+
+const SpecificClass = () => {
+  return (
+    <div className="bg-hBeige min-h-screen">
+      <NavBar />
+    </div>
+  );
+};
