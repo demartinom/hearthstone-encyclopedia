1db3f70307ee7233ec7566f790054483d04b58db
“Matt <matt.s.demartino@gmail.com>
2023-04-27 12:23:17 -0400

    Create classes page

---
+++ next/pages/classes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "@/components/navBar";
+
+const Classes = () => {
+  return (
+    <div className="bg-hBeige min-h-screen">
+      <NavBar />
+    </div>
+  );
+};
+
+export default Classes;
