7735aa2e2ccc04276ee00944cf7306b764f144dd
“Matt <matt.s.demartino@gmail.com>
2023-04-28 13:22:32 -0400

    Create more specific api call

--- next/pages/classes/[slug].js
+++ next/pages/classes/[slug].js
@@ -4,25 +4,28 @@ import axios from "axios";
 import Image from "next/image";
 
 const SpecificClass = ({ classInfo }) => {
-  let cardImages = classInfo.map((gameClass) => (
-    <Image
-      src={`${gameClass.image}`}
-      width={100}
-      height={100}
-      alt="card"
-    ></Image>
-  ));
+  let cardImages = classInfo
+    .filter((gameCard) => !gameCard.copyOfCardId)
+    // .filter((gameCard) => gameCard.cardTypeId != 4)
+    .map((gameCard) => (
+      <Image
+        src={`${gameCard.image}`}
+        width={200}
+        height={200}
+        alt="card"
+      ></Image>
+    ));
   return (
     <div className="bg-hBeige min-h-screen">
       <NavBar />
-      {cardImages}
+      <div className="grid grid-cols-5">{cardImages}</div>
     </div>
   );
 };
 
 export async function getServerSideProps(context) {
   const res = await axios.get(
-    `https://us.api.blizzard.com/hearthstone/cards?locale=en_US&class=${context.params.slug}&collectible=1&pageSize=999&access_token=${process.env.API_TOKEN}`
+    `https://us.api.blizzard.com/hearthstone/cards?locale=en_US&class=${context.params.slug}&set=standard&sort=manaCost:asc&collectible=1&pageSize=999&access_token=${process.env.API_TOKEN}`
   );
   return { props: { classInfo: JSON.parse(JSON.stringify(res.data.cards)) } };
 }
