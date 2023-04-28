000ffa2cb11595e6d7d1272c83bfdd6467635ae8
“Matt <matt.s.demartino@gmail.com>
2023-04-28 14:30:53 -0400

    Add class name + begin separating standard + wild

--- next/pages/classes/[slug].js
+++ next/pages/classes/[slug].js
@@ -3,10 +3,17 @@ import NavBar from "@/components/navBar";
 import axios from "axios";
 import Image from "next/image";
 
-const SpecificClass = ({ classInfo }) => {
-  let cardImages = classInfo
+const SpecificClass = ({ standardCards, wildCards, playClass }) => {
+  let standardImages = standardCards.map((gameCard) => (
+    <Image
+      src={`${gameCard.image}`}
+      width={200}
+      height={200}
+      alt="card"
+    ></Image>
+  ));
+  let wildImages = wildCards
     .filter((gameCard) => !gameCard.copyOfCardId)
-    // .filter((gameCard) => gameCard.cardTypeId != 4)
     .map((gameCard) => (
       <Image
         src={`${gameCard.image}`}
@@ -15,19 +22,40 @@ const SpecificClass = ({ classInfo }) => {
         alt="card"
       ></Image>
     ));
+  const currentClass =
+    playClass == "deathknight"
+      ? "Death Knight"
+      : playClass == "demonhunter"
+      ? "Demon Hunter"
+      : `${playClass[0].toUpperCase()}${playClass.substring(1)}`;
+  const [currentCards, setCurrentCards] = React.useState("standard");
   return (
     <div className="bg-hBeige min-h-screen">
       <NavBar />
-      <div className="grid grid-cols-5">{cardImages}</div>
+      <h1>{currentClass}</h1>
+      {currentCards == "standard" ? (
+        <div className="grid grid-cols-5">{standardImages}</div>
+      ) : (
+        <div className="grid grid-cols-5">{wildImages}</div>
+      )}
     </div>
   );
 };
 
 export async function getServerSideProps(context) {
-  const res = await axios.get(
+  const standard = await axios.get(
     `https://us.api.blizzard.com/hearthstone/cards?locale=en_US&class=${context.params.slug}&set=standard&sort=manaCost:asc&collectible=1&pageSize=999&access_token=${process.env.API_TOKEN}`
   );
-  return { props: { classInfo: JSON.parse(JSON.stringify(res.data.cards)) } };
+  const wild = await axios.get(
+    `https://us.api.blizzard.com/hearthstone/cards?locale=en_US&class=${context.params.slug}&set=wild&sort=manaCost:asc&collectible=1&pageSize=999&access_token=${process.env.API_TOKEN}`
+  );
+  return {
+    props: {
+      standardCards: JSON.parse(JSON.stringify(standard.data.cards)),
+      wildCards: JSON.parse(JSON.stringify(wild.data.cards)),
+      playClass: JSON.parse(JSON.stringify(context.params.slug)),
+    },
+  };
 }
 
 export default SpecificClass;
