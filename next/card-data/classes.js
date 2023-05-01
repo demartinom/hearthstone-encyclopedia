94953e83e2e760af88666194e0fca69aea715b5e
“Matt <matt.s.demartino@gmail.com>
2023-05-01 16:25:25 -0400

    Create array with class codes

---
+++ next/card-data/classes.js
@@ -0,0 +1,16 @@
+const classCodes = [
+  { id: 1, cardClassName: "Death Knight" },
+  { id: 2, cardClassName: "Druid" },
+  { id: 3, cardClassName: "Hunter" },
+  { id: 4, cardClassName: "Mage" },
+  { id: 5, cardClassName: "Paladin" },
+  { id: 6, cardClassName: "Priest" },
+  { id: 7, cardClassName: "Rogue" },
+  { id: 8, cardClassName: "Shaman" },
+  { id: 9, cardClassName: "Warlock" },
+  { id: 10, cardClassName: "Warrior" },
+  { id: 12, cardClassName: "Neutral" },
+  { id: 14, cardClassName: "Demon Hunter" },
+];
+
+export default classCodes;
