0b75b9daab9f6139b56e43ecff8ca383ea631981
“Matt <matt.s.demartino@gmail.com>
2023-05-01 16:03:40 -0400

    Begin adding info to page

--- next/pages/cards/[slug].js
+++ next/pages/cards/[slug].js
@@ -1,11 +1,22 @@
 import React from "react";
 import NavBar from "@/components/navBar";
 import axios from "axios";
+import Image from "next/image";
 
 const SingleCard = ({ cardInfo }) => {
+  console.log(cardInfo.text);
   return (
     <div className="bg-hBeige min-h-screen">
       <NavBar />
+      <div className="flex justify-center mt-20">
+        <div className="flex items-center">
+          <Image src={cardInfo.image} width={400} height={400} alt='Card image'></Image>
+          <div>
+            <h1>{cardInfo.name}</h1>
+            <p dangerouslySetInnerHTML={{ __html: cardInfo.text }}></p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
