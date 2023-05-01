915ca44cc70dca3ccdc761cc228d5338ef6dc8ef
“Matt <matt.s.demartino@gmail.com>
2023-05-01 15:45:22 -0400

    Create api call for single card

--- next/pages/cards/[slug].js
+++ next/pages/cards/[slug].js
@@ -1,12 +1,22 @@
 import React from "react";
 import NavBar from "@/components/navBar";
+import axios from "axios";
 
-const SingleCard = () => {
+const SingleCard = ({ cardInfo }) => {
   return (
     <div className="bg-hBeige min-h-screen">
       <NavBar />
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  const res = await axios.get(
+    `https://us.api.blizzard.com/hearthstone/cards/${context.params.slug}?locale=en_US&access_token=${process.env.API_TOKEN}`
+  );
+  return {
+    props: { cardInfo: JSON.parse(JSON.stringify(res.data)) },
+  };
+}
 
 export default SingleCard;
