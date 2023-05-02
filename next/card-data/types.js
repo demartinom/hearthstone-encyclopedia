13794a2164502931ba2c0bd8b43ea9422a549f84
“Matt <matt.s.demartino@gmail.com>
2023-05-02 15:50:42 -0400

    Add api data to match codes to names

---
+++ next/card-data/types.js
@@ -0,0 +1,153 @@
+const typeCodes = [
+  {
+    slug: "bloodelf",
+    id: 1,
+    name: "Blood Elf",
+    gameModes: [7],
+  },
+  {
+    slug: "draenei",
+    id: 2,
+    name: "Draenei",
+    gameModes: [7],
+  },
+  {
+    slug: "dwarf",
+    id: 3,
+    name: "Dwarf",
+    gameModes: [7],
+  },
+  {
+    slug: "gnome",
+    id: 4,
+    name: "Gnome",
+    gameModes: [7],
+  },
+  {
+    slug: "human",
+    id: 6,
+    name: "Human",
+    gameModes: [7],
+  },
+  {
+    slug: "nightelf",
+    id: 7,
+    name: "Night Elf",
+    gameModes: [7],
+  },
+  {
+    slug: "orc",
+    id: 8,
+    name: "Orc",
+    gameModes: [7],
+  },
+  {
+    slug: "tauren",
+    id: 9,
+    name: "Tauren",
+    gameModes: [7],
+  },
+  {
+    slug: "troll",
+    id: 10,
+    name: "Troll",
+    gameModes: [7],
+  },
+  {
+    slug: "undead",
+    id: 11,
+    name: "Undead",
+    gameModes: [1, 2, 4, 5, 6, 7],
+  },
+  {
+    slug: "murloc",
+    id: 14,
+    name: "Murloc",
+    gameModes: [1, 2, 4, 5, 6, 7],
+  },
+  {
+    slug: "demon",
+    id: 15,
+    name: "Demon",
+    gameModes: [1, 2, 4, 5, 6, 7],
+  },
+  {
+    slug: "mech",
+    id: 17,
+    name: "Mech",
+    gameModes: [1, 2, 4, 5, 6],
+  },
+  {
+    slug: "elemental",
+    id: 18,
+    name: "Elemental",
+    gameModes: [1, 2, 4, 5, 7],
+  },
+  {
+    slug: "beast",
+    id: 20,
+    name: "Beast",
+    gameModes: [1, 2, 4, 5, 6, 7],
+  },
+  {
+    slug: "totem",
+    id: 21,
+    name: "Totem",
+    gameModes: [1, 4, 5, 6],
+  },
+  {
+    slug: "pirate",
+    id: 23,
+    name: "Pirate",
+    gameModes: [1, 2, 4, 5, 6],
+  },
+  {
+    slug: "dragon",
+    id: 24,
+    name: "Dragon",
+    gameModes: [1, 2, 4, 5, 6, 7],
+  },
+  {
+    slug: "all",
+    id: 26,
+    name: "All",
+    gameModes: [1, 2, 4, 5],
+  },
+  {
+    slug: "quilboar",
+    id: 43,
+    name: "Quilboar",
+    gameModes: [1, 2, 4, 5],
+  },
+  {
+    slug: "halforc",
+    id: 88,
+    name: "Half-Orc",
+  },
+  {
+    slug: "naga",
+    id: 92,
+    name: "Naga",
+    gameModes: [1, 2, 4, 5, 7],
+  },
+  {
+    slug: "oldgod",
+    id: 93,
+    name: "Old God",
+    gameModes: [7],
+  },
+  {
+    slug: "pandaren",
+    id: 94,
+    name: "Pandaren",
+    gameModes: [7],
+  },
+  {
+    slug: "gronn",
+    id: 95,
+    name: "Gronn",
+    gameModes: [7],
+  },
+];
+
+export default typeCodes;
