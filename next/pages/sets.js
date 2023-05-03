cc6bf82fcab5b295cb4078bc6d76dec8d76c0d46
“Matt <matt.s.demartino@gmail.com>
2023-05-03 13:15:33 -0400

    Create setInfo api call

--- next/pages/sets.js
+++ next/pages/sets.js
@@ -1,12 +1,26 @@
 import React from "react";
 import NavBar from "@/components/navBar";
+import axios from "axios";
 
-const Sets = () => {
+const Sets = ({ setInfo }) => {
+  const setNames = setInfo.map((cardSet) => <h1>{cardSet.name}</h1>);
   return (
     <div className="bg-hBeige min-h-screen">
       <NavBar />
+      {setNames}
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const res = await axios.get(
+    `https://us.api.blizzard.com/hearthstone/metadata/sets?locale=en_US&access_token=${process.env.API_TOKEN}`
+  );
+  return {
+    props: {
+      setInfo: JSON.parse(JSON.stringify(res.data)),
+    },
+  };
+}
 
 export default Sets;
