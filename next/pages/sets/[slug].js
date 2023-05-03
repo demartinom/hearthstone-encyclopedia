a1ae2c939339a6d28ae536b8750f783449a42968
“Matt <matt.s.demartino@gmail.com>
2023-05-03 16:06:36 -0400

    Create api call for set cards

--- next/pages/sets/[slug].js
+++ next/pages/sets/[slug].js
@@ -1,12 +1,20 @@
 import React from "react";
 import NavBar from "@/components/navBar";
+import axios from "axios";
 
-const GameSet = () => {
+const GameSet = ({ setData }) => {
   return (
     <div className="bg-hBeige min-h-screen">
       <NavBar />
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  const setData = await axios.get(
+    `https://us.api.blizzard.com/hearthstone/cards?locale=en_US&set=${context.params.slug}&sort=groupByClass%3Aasc&pageSize=999&access_token=${process.env.API_TOKEN}`
+  );
+  return { props: { setData: JSON.parse(JSON.stringify(setData.data.cards)) } };
+}
 
 export default GameSet;
