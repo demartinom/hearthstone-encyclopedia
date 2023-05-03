66ffd16167f83569e1f10dca955ceb8f05943292
“Matt <matt.s.demartino@gmail.com>
2023-05-03 13:12:52 -0400

    Add NavBar

--- next/pages/sets.js
+++ next/pages/sets.js
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "@/components/navBar";
 
 const Sets = () => {
-  return <div></div>;
+  return (
+    <div className="bg-hBeige min-h-screen">
+      <NavBar />
+    </div>
+  );
 };
 
 export default Sets;
