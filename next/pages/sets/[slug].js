eed27d13dd58b81f596eef1e456ada173778f248
“Matt <matt.s.demartino@gmail.com>
2023-05-03 15:44:53 -0400

    Import NavBar

--- next/pages/sets/[slug].js
+++ next/pages/sets/[slug].js
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "@/components/navBar";
 
 const GameSet = () => {
-  return <div></div>;
+  return (
+    <div className="bg-hBeige min-h-screen">
+      <NavBar />
+    </div>
+  );
 };
 
 export default GameSet;
