3fb9e18aa649adc29542a668d2e84484b021e337
“Matt <matt.s.demartino@gmail.com>
2023-05-03 13:32:39 -0400

    Add groupInfo api call and map over sets

--- next/pages/sets.js
+++ next/pages/sets.js
@@ -2,23 +2,42 @@ import React from "react";
 import NavBar from "@/components/navBar";
 import axios from "axios";
 
-const Sets = ({ setInfo }) => {
-  const setNames = setInfo.map((cardSet) => <h1>{cardSet.name}</h1>);
+const Sets = ({ setInfo, groupInfo }) => {
+  const standardSets = setInfo
+    .filter((cardSet) => groupInfo[9].cardSets.includes(cardSet.slug))
+    .map((cardSet) => <h1>{cardSet.name}</h1>);
+  const wildSets = setInfo
+    .filter((cardSet) => groupInfo[10].cardSets.includes(cardSet.slug))
+    .filter((cardSet) => !groupInfo[9].cardSets.includes(cardSet.slug))
+    .map((cardSet) => <h1>{cardSet.name}</h1>);
   return (
     <div className="bg-hBeige min-h-screen">
       <NavBar />
-      {setNames}
+      <div>
+        <h1>Standard Sets</h1>
+        {standardSets}
+      </div>
+      <div>
+        <h1>Wild Sets</h1>
+        {wildSets}
+      </div>
     </div>
   );
 };
 
 export async function getServerSideProps() {
-  const res = await axios.get(
-    `https://us.api.blizzard.com/hearthstone/metadata/sets?locale=en_US&access_token=${process.env.API_TOKEN}`
-  );
+  const [setInfoRes, groupInfoRes] = await Promise.all([
+    axios.get(
+      `https://us.api.blizzard.com/hearthstone/metadata/sets?locale=en_US&access_token=${process.env.API_TOKEN}`
+    ),
+    axios.get(
+      `https://us.api.blizzard.com/hearthstone/metadata/setGroups?locale=en_US&access_token=${process.env.API_TOKEN}`
+    ),
+  ]);
   return {
     props: {
-      setInfo: JSON.parse(JSON.stringify(res.data)),
+      setInfo: JSON.parse(JSON.stringify(setInfoRes.data)),
+      groupInfo: JSON.parse(JSON.stringify(groupInfoRes.data)),
     },
   };
 }
