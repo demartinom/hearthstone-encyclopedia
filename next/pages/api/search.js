b863ad2fa0ee2bb3f235b5d95295e39756d158db
“Matt <matt.s.demartino@gmail.com>
2023-05-05 14:42:10 -0400

    Return array of cards from api call

--- next/pages/api/search.js
+++ next/pages/api/search.js
@@ -3,9 +3,9 @@ import axios from "axios";
 export default async (req, res) => {
   try {
     const returnedData = await axios.get(
-      `https://us.api.blizzard.com/hearthstone/cards?locale=en_US&textFilter=kalecgos&pageSize=20&access_token=${process.env.API_TOKEN}`
+      `https://us.api.blizzard.com/hearthstone/cards?locale=en_US&textFilter=${req.query.name}&pageSize=20&access_token=${process.env.API_TOKEN}`
     );
-    res.json({ data: returnedData.data });
+    res.send(returnedData.data.cards);
   } catch (e) {
     console.log(e);
   }
