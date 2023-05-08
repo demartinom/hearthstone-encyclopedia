5e8204f6bd36a4e257d836f011f2c8e66ebe3519
“Matt <matt.s.demartino@gmail.com>
2023-05-08 14:08:58 -0400

    Create favorites page

---
+++ next/pages/favorites.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "@/components/navBar";
+
+const Favorites = () => {
+  return (
+    <div className="bg-hBeige min-h-screen">
+      <NavBar />
+    </div>
+  );
+};
+
+export default Favorites;
