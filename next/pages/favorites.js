14413cc04fa9650acc145bc552cb05effd4e0252
“Matt <matt.s.demartino@gmail.com>
2023-05-09 14:25:30 -0400

    Import favorites context and map over favorites

--- next/pages/favorites.js
+++ next/pages/favorites.js
@@ -1,10 +1,20 @@
 import React from "react";
 import NavBar from "@/components/navBar";
+import { useAppContext } from "@/state";
+import Image from "next/image";
+import Link from "next/link";
 
 const Favorites = () => {
+  const { favorites, setFavorites } = useAppContext();
+  const favoritesList = favorites.map((card) => (
+    <Link href={`/cards/${card.slug}`}>
+      <Image src={card.image} width={250} height={250}></Image>
+    </Link>
+  ));
   return (
     <div className="bg-hBeige min-h-screen">
       <NavBar />
+      {favoritesList}
     </div>
   );
 };
