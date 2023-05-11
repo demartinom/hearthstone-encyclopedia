832bf4a995d3f88c8e95b2d508ce123f1a872bbb
“Matt <matt.s.demartino@gmail.com>
2023-05-11 15:31:17 -0400

    Remove old app files

--- src/API-Options.js
+++
@@ -1,7 +0,0 @@
-export const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "df110cbc06msh276bdc41bf08ef9p163c3ajsne9ff8ac94d17",
-      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
-    },
-  };
